74e7afe95247ed508732a15d634149aaa1683775
Darya <Martyushova-DN@ya.ru>
2021-03-01 11:43:56 +0500

    now the color is random

--- main.py
+++ main.py
@@ -1,18 +1,45 @@
-import io
-
-from PyQt5 import uic
-from PyQt5.QtGui import QPixmap, QImage, QPainter, QColor
-from PyQt5.QtWidgets import QMainWindow, QApplication, QInputDialog
 import sys
 from random import randint
 
-YELLOW = QColor(255, 255, 0)
+from PyQt5 import QtWidgets, QtCore
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtWidgets import QMainWindow, QApplication
 
 
-class MyWidget(QMainWindow):
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(863, 616)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.gridLayout_2 = QtWidgets.QGridLayout(self.centralwidget)
+        self.gridLayout_2.setObjectName("gridLayout_2")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setMaximumSize(QtCore.QSize(100, 16777215))
+        self.pushButton.setObjectName("pushButton")
+        self.gridLayout_2.addWidget(self.pushButton, 0, 0, 1, 1)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 863, 26))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "circles"))
+        self.pushButton.setText(_translate("MainWindow", "draw"))
+
+
+class MyWidget(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.setupUi(self)
         self.do_paint = False
         self.pushButton.clicked.connect(self.draw)
 
@@ -29,8 +56,8 @@ class MyWidget(QMainWindow):
         self.do_paint = False
 
     def draw_circ(self, qp):
-        qp.setBrush(YELLOW)
-        radius = randint(0, min(self.width(), self.height())//2)
+        qp.setBrush(QColor(randint(0, 255), randint(0, 255), randint(0, 255)))
+        radius = randint(0, min(self.width(), self.height()) // 2)
         x, y = randint(1, self.width() - radius), randint(1, self.height() - radius)
         qp.drawEllipse(x, y, radius * 2, radius * 2)
 
